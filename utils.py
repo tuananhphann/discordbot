9ed1fd72c19b4fb4ed8f88fcc522c323be1d732a
Tuan Anh <phantuananh0202@gmail.com>
2022-10-09 18:37:52 +0700

    Add setup_logger function, Timer class.

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+import asyncio
 import datetime
+import logging
+import logging.handlers
 import os
 
 from dotenv import find_dotenv, load_dotenv
@@ -26,6 +29,19 @@ def get_env(key: str) -> str:
     TOKEN = os.environ.get("TOKEN")
     return TOKEN
 
+def setup_logger(name: str, level = logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.handlers.RotatingFileHandler(
+        filename=constants.CUR_PATH+'/discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
+    )
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 def cleanup():
     """Clean garbages after bot end."""
@@ -40,3 +56,19 @@ def cleanup():
         print("Cleanup completed.")
     except:
         print("There are some errors when trying to delete garbages.")
+
+
+class Timer:
+    """Auto execute a task when the time is out.
+    This designed for auto disconnect feature."""
+    def __init__(self, callback) -> None:
+        self.__callback = callback
+        self.__task = asyncio.create_task(self.__job())
+
+    async def __job(self):
+        await asyncio.sleep(constants.VOICE_TIMEOUT)
+        await self.__callback
+
+    def cancel(self):
+        self.__task.cancel()
+
