fee2f0bb9ee55c4a345a8cdc655993f6b0f79bfa
Tuan Anh <phantuananh0202@gmail.com>
2022-10-09 18:31:45 +0700

    Add logging, docstring for 's' and 'lang' command, add 'lang' command.

--- cogs/tts/tts.py
+++ cogs/tts/tts.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 
 import constants
@@ -6,41 +7,67 @@ import discord
 import gtts
 from discord.ext import commands
 from gtts import gTTS
+from cogs.components.discord_embed import Embed
 
 langs = gtts.tts.tts_langs()
 
+_log = logging.getLogger(__name__)
 class TTS(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.path = constants.TTS_PATH
         self.file_name = r"\voice.mp3"
         self.full_path = self.path + self.file_name
-        try: os.mkdir(self.path)
+        try:
+            os.mkdir(self.path)
+            _log.info(f"{self.path} is created successfully.")
         except: None
     
     async def tts(self, ctx, text: str, lang: str = "vi"):
         loop = asyncio.get_event_loop()
-        tts = await loop.run_in_executor(None, lambda: gTTS(text = text, lang = lang))
-        tts.save(self.full_path)
+        try:
+            tts = await loop.run_in_executor(None, lambda: gTTS(text = text, lang = lang))
+            tts.save(self.full_path)
+        except Exception as e:
+            _log.error(e)
+            return
+        _log.info(f"'voice.mp3' file has been saved at {self.path}.")
         source = discord.FFmpegPCMAudio(self.full_path)
         ctx.voice_client.play(source)
 
     @commands.command()
-    async def s(self, ctx, *args):
+    async def s(self, ctx, *text):
+        """
+        You can change the speaker's language using the language in the list provided by the 'lang' command.
+        To ensure performance of this bot. Max characters can be speak at a time are 300.
+        """
+
         if ctx.voice_client.is_playing():
             await ctx.send("You must wait until the speaker is free.")
         else:
-            if args[0] in langs.keys():
-                lang = args[0]
-                text = ' '.join(args)[3:]
+            if text[0] in langs.keys():
+                lang = text[0]
+                text = ' '.join(text)[3:]
             else:
                 lang = 'vi'
-                text = ' '.join(args)
+                text = ' '.join(text)
                         
             if len(text) > 300:
+                _log.error(f"Can not ready for '{text[:80]}...' (300 limit)")
                 await ctx.send("The number of characters exceeds the 300 character limit.")
             else:
+                if len(text) < 100:
+                    _log.info(f"Ready for '{text}' at '{lang}'.")
+                else:
+                    _log.info(f"Ready for '{text[:80]}...' at '{lang}'.")
                 await self.tts(ctx, text, lang)
+    
+    @commands.command()
+    async def lang(self, ctx):
+        """Return a list of languages avaiable."""
+        embed = Embed(ctx).tts_lang(langs)
+        await ctx.send(embed = embed)
+
 
     @s.before_invoke
     async def ensure_voice(self, ctx: commands.Context):
@@ -49,8 +76,8 @@ class TTS(commands.Cog):
                 await ctx.author.voice.channel.connect(self_deaf=True)
             else:
                 await ctx.send("You are not connected to a voice channel.")
-                raise commands.CommandError(
-                    "Author not connected to a voice channel.")
+                # raise commands.CommandError(
+                #     "Author not connected to a voice channel.")
 
 
 
