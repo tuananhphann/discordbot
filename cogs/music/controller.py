729c774e46c3b68497e4dfd96814452d0fe8f3c1
Tuan Anh <phantuananh0202@gmail.com>
2022-10-09 18:26:21 +0700

    Add logging, auto disconnect.

--- cogs/music/controller.py
+++ cogs/music/controller.py
@@ -1,3 +1,5 @@
+import logging
+
 import constants
 import discord
 from cogs.components.discord_embed import Embed
@@ -6,6 +8,7 @@ from cogs.music.search import Search, Song
 from discord.ext import commands
 from utils import *
 
+_log = logging.getLogger(__name__)
 
 class Audio:
     def __init__(self, bot: commands.Bot) -> None:
@@ -14,6 +17,8 @@ class Audio:
         self.current_song = None
         self.current_song_duration = None
         self.start_time = None
+        self.ctx = None
+        self.timer = Timer(self.timeout_handle(self.ctx))
 
     def play_next(self, ctx):
         song = self.playlist.get_next()
@@ -27,22 +32,24 @@ class Audio:
         self.start_time = convert_to_second(get_time())
         self.current_song_duration = convert_to_second(song.DURATION)
         
+        self.timer.cancel()
+        self.timer = Timer(self.timeout_handle(ctx))
         source = discord.FFmpegPCMAudio(song.URL, **constants.FFMPEG_OPTIONS)
         embed = Embed(ctx).now_playing_song(song)
-        await ctx.defer(ephemeral=True)
-        await ctx.reply(embed = embed)
+        await ctx.send(embed = embed)
         # await ctx.reply(embed=embed)
         ctx.voice_client.play(source, after=lambda e: self.play_next(ctx))
         self.current_song = song.TITLE
 
     async def process_track(self, ctx: commands.Context, track: str, priority: bool = False):
+        self.ctx = ctx
         song = await Search().query(track)
-        if type(song) == str:
-            embed = Embed(ctx).error(song)
-            await ctx.send(embed = embed)
+        if song == -1:
+            _log.error(f"Can not play this song '{track}'")
             return
         
         if self.current_song == None:
+            _log.info(f"Starting to play this song '{song.TITLE}' from query '{track}'.")
             await self.play(ctx, song)
         else:
             if priority == True:
@@ -50,6 +57,7 @@ class Audio:
                 time_wait = self.current_song_duration - (current_time - self.start_time)
 
                 self.playlist.add_next(song)
+                _log.info(f"Added to playlist '{song.TITLE}' from query '{track}' at 'Next' position.")
                 embed = Embed(ctx).add_song(song, self.playlist.index(song)+1, f"Next, about {convert_to_time(time_wait)}")
                 await ctx.send(embed=embed)
             else:
@@ -57,5 +65,15 @@ class Audio:
                 time_wait = self.current_song_duration - (current_time - self.start_time) + convert_to_second(self.playlist.time_wait())
                 
                 self.playlist.add(song)
+                _log.info(f"Added to playlist '{song.TITLE}' from query '{track}' at position '{self.playlist.index(song)+1}'.")
                 embed = Embed(ctx).add_song(song, position = self.playlist.index(song)+1, timewait = convert_to_time(time_wait))
                 await ctx.send(embed=embed)
+
+    async def timeout_handle(self, ctx: commands.Context):
+        if ctx.voice_client.is_playing():
+            self.timer.cancel()
+            self.timer = Timer(self.timeout_handle(ctx))
+            _log.info("Timer has been reset because discord.voice_client is still playing.")
+        else:
+            await ctx.voice_client.disconnect()
+            _log.info(f"Disconnected from voice channel due to timeout. ID: {ctx.author.voice.channel.id}")
