d7fe3530b2eab2db87739d6e70e6f03f1a21dc98
= <=>
2023-08-20 19:07:52 +0700

    Add some changes.

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import asyncio
 import discord
 from discord.ext import commands
 
-import cogs
 from cogs.greetings import Greeting
 from cogs.music.music import Music
 from cogs.tts.tts import TTS
 from cogs.game_free.game_free import GameFree
-from utils import cleanup, get_env, setup_logger
+from .utils import cleanup, get_env, setup_logger
 
 
 class Bot(commands.Bot):
-    def __init__(self):
-        intents = discord.Intents.default()
+    def __init__(self) -> None:
+        intents: discord.Intents = discord.Intents.default()
         intents.message_content = True
         super().__init__(command_prefix = "?", intents=intents)
 
@@ -21,31 +20,31 @@ class Bot(commands.Bot):
     #     await self.tree.sync()
     #     print(f"Synced slash command for {self.user}")
 
-    async def on_command_error(self, ctx, error):
+    async def on_command_error(self, ctx, error) -> None:
         await ctx.reply(error, ephemeral = True)
 
 bot = Bot()
 
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
     msg = f"Logged in as {bot.user} (ID: {bot.user.id})"
     print(msg)
     print("-"*len(msg))
 
 
-async def main():
-    setup_logger('discord')
-    setup_logger('cogs', level = 10)
+async def main() -> None:
+    setup_logger(name='discord')
+    setup_logger(name='cogs', level = 10)
     async with bot:
-        TOKEN = get_env('TOKEN')
+        token: str = get_env(key='TOKEN')
         await bot.add_cog(Music(bot))
         await bot.add_cog(Greeting(bot))
         await bot.add_cog(TTS(bot))
         await bot.add_cog(GameFree(bot))
-        await bot.start(TOKEN)
+        await bot.start(token=token)
 
 try:
-    asyncio.run(main())
+    asyncio.run(main=main())
 except KeyboardInterrupt:
     print("Bot ended by host.")
 except Exception as e:
