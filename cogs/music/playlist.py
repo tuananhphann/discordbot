5e502d1ce8932417f18394b006bc45b95074010e
= <=>
2023-08-20 18:57:13 +0700

    Add type to function argument.

--- cogs/music/playlist.py
+++ cogs/music/playlist.py
@@ -1,35 +1,36 @@
 from collections import deque
 from utils import *
+from cogs.music.song import Song
 
 class PlayList:
     def __init__(self) -> None:
         self.q = deque()
     
-    def add(self, song):
+    def add(self, song: Song) -> None:
         self.q.append(song)
     
-    def add_next(self, song):
+    def add_next(self, song: Song) -> None:
         self.q.appendleft(song)
     
-    def index(self, song):
+    def index(self, song: Song) -> int:
         """Index of song in queue."""
         return self.q.index(song)
     
-    def size(self):
+    def size(self) -> int:
         return len(self.q)
     
-    def clear(self):
+    def clear(self) -> None:
         self.q.clear()
 
-    def time_wait(self):
+    def time_wait(self) -> str:
         sec = 0
         for i in range(len(self.q)):
-            sec += convert_to_second(self.q[i].DURATION)
-        return convert_to_time(sec)
+            sec += convert_to_second(time=self.q[i].DURATION)
+        return convert_to_time(seconds=sec)
     
     def get_list(self):
         return self.q
     
-    def get_next(self):
+    def get_next(self) -> Song:
         try: return self.q.popleft()
         except: return None
