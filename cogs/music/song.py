c80eb0792fcfc635c60898c7f765a8bc70b8e5e1
Tuấn Anh <phantuananh0202@gmail.com>
2023-08-25 04:23:53 +0700

    Reformat

--- cogs/music/song.py
+++ cogs/music/song.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 
+
 class Song:
     """
     Song object
@@ -14,12 +15,23 @@ class Song:
         THUMBNAIL   (str): URL to the thumbnail.
         YT_URL      (str): Direct URL to this song's Youtube page.
         CTX         (discord.ext.commands.Context): Context
-    
+
     method:
         info (dict): Returns the entire property but as a dict.
     """
 
-    def __init__(self, TITLE: str, URL: str, CHANNEL: str, VIEW_COUNT: str, DURATION: str, UPLOAD_DATE: str, THUMBNAIL: str, YT_URL: str, CTX: commands.Context) -> None:
+    def __init__(
+        self,
+        TITLE: str,
+        URL: str,
+        CHANNEL: str,
+        VIEW_COUNT: str,
+        DURATION: str,
+        UPLOAD_DATE: str,
+        THUMBNAIL: str,
+        YT_URL: str,
+        CTX: commands.Context,
+    ) -> None:
         self.TITLE: str = TITLE
         self.URL: str = URL
         self.CHANNEL: str = CHANNEL
@@ -42,13 +54,13 @@ class Song:
     def info(self) -> dict[str, str]:
         "another way to access song info"
         song: dict[str, str] = {
-            'TITLE': self.TITLE,
-            'URL': self.URL,
-            'CHANNEL': self.CHANNEL,
-            'VIEW_COUNT': self.VIEW_COUNT,
-            'DURATION': self.DURATION,
-            'UPLOAD_DATE': self.UPLOAD_DATE,
-            'THUMBNAIL': self.THUMBNAIL,
-            'YT_URL': self.YT_URL,
+            "TITLE": self.TITLE,
+            "URL": self.URL,
+            "CHANNEL": self.CHANNEL,
+            "VIEW_COUNT": self.VIEW_COUNT,
+            "DURATION": self.DURATION,
+            "UPLOAD_DATE": self.UPLOAD_DATE,
+            "THUMBNAIL": self.THUMBNAIL,
+            "YT_URL": self.YT_URL,
         }
         return song
