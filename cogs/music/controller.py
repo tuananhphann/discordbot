7f0fba50ed992135ac260e327f29ce416b79e0f1
Tuấn Anh <phantuananh0202@gmail.com>
2023-12-20 14:38:06 +0700

    improve stability
    fix RuntimeWarning message when bot finishes and reformat the code for better reading

--- cogs/music/controller.py
+++ cogs/music/controller.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Union
 
 import discord
 from discord.ext import commands
@@ -7,7 +8,7 @@ import constants
 from cogs.components.discord_embed import Embed
 from cogs.music.playlist import PlayList
 from cogs.music.search import Search, Song
-from utils import *
+from utils import Timer, convert_to_second, convert_to_time, get_time
 
 _log = logging.getLogger(__name__)
 
@@ -17,33 +18,33 @@ class Audio:
         self.bot = bot
         self.playlist = PlayList()
         self.current_song = None
-        self.current_song_duration = None
-        self.start_time = None
+        self.current_song_duration = 0.0
+        self.start_time = 0.0
         self.ctx = None
-        self.timer = Timer(callback=self.timeout_handle(ctx=self.ctx))
+        self.timer = Timer(callback=self.timeout_handle, ctx=self.ctx)
 
     def play_next(self) -> None:
         song: Song = self.playlist.get_next()
-        if song == None:
+        if song is None:
             self.current_song = None
             return
         coro = self.play(song=song)
         self.bot.loop.create_task(coro)
 
     async def play(self, song: Song) -> None:
-        ctx = song.CTX
-        self.start_time: float = convert_to_second(get_time())
-        self.current_song_duration = convert_to_second(song.DURATION)
+        ctx = song.context
+        self.start_time = convert_to_second(get_time())
+        self.current_song_duration = convert_to_second(song.duration)
 
         self.timer.cancel()
-        self.timer = Timer(self.timeout_handle(ctx))
+        self.timer = Timer(self.timeout_handle, ctx=ctx)
 
-        source = discord.FFmpegPCMAudio(song.URL, **constants.FFMPEG_OPTIONS)
+        source = discord.FFmpegPCMAudio(song.playback_url, **constants.FFMPEG_OPTIONS)
         embed = Embed(ctx).now_playing_song(song)
         await ctx.reply(embed=embed)
         # await ctx.reply(embed=embed)
-        ctx.voice_client.play(source, after=lambda e: self.play_next())
-        self.current_song = song.TITLE
+        ctx.voice_client.play(source, after=lambda x: self.play_next())
+        self.current_song = song.title
 
     async def process_track(
         self, ctx: commands.Context, track: str, priority: bool = False
@@ -54,13 +55,13 @@ class Audio:
             _log.error(f"Can not play this song '{track}'")
             return
 
-        if self.current_song == None:
+        if self.current_song is None:
             _log.info(
-                f"Starting to play this song '{song.TITLE}' from query '{track}'."
+                f"Starting to play this song '{song.title}' from query '{track}'."
             )
             await self.play(song)
         else:
-            if priority == True:
+            if priority:
                 current_time = convert_to_second(get_time())
                 time_wait = self.current_song_duration - (
                     current_time - self.start_time
@@ -68,7 +69,7 @@ class Audio:
 
                 self.playlist.add_next(song)
                 _log.info(
-                    f"Added to playlist '{song.TITLE}' from query '{track}' at 'Next' position."
+                    f"Added to playlist '{song.title}' from query '{track}' at 'Next' position."
                 )
                 embed = Embed(ctx).add_song(
                     song,
@@ -86,7 +87,7 @@ class Audio:
 
                 self.playlist.add(song)
                 _log.info(
-                    f"Added to playlist '{song.TITLE}' from query '{track}' at position '{self.playlist.index(song)+1}'."
+                    f"Added to playlist '{song.title}' from query '{track}' at position '{self.playlist.index(song)+1}'."
                 )
                 embed = Embed(ctx).add_song(
                     song,
@@ -95,10 +96,12 @@ class Audio:
                 )
                 await ctx.send(embed=embed)
 
-    async def timeout_handle(self, ctx: commands.Context) -> None:
+    async def timeout_handle(self, ctx: Union[commands.Context, None]) -> None:
+        if ctx is None:
+            return
         if ctx.voice_client.is_playing():
             self.timer.cancel()
-            self.timer = Timer(callback=self.timeout_handle(ctx))
+            self.timer = Timer(callback=self.timeout_handle, ctx=ctx)
             _log.info(
                 "Timer has been reset because discord.voice_client is still playing."
             )
