1d2eaf41a6b4f81079c18cdabffa98afc5709144
Tuấn Anh <phantuananh0202@gmail.com>
2024-03-19 00:28:47 +0700

    feat(music): :sparkles: add SoundCloud class
    add SoundCloud class to handle queries to SoundCloud

--- cogs/music/soundcloud.py
+++ cogs/music/soundcloud.py
@@ -1,125 +1,115 @@
-import datetime
-# from album import Album
-from typing import Any, Dict
+import asyncio
+import functools
+import json
+from typing import TYPE_CHECKING, Any, Dict
 
 import requests
 from fake_useragent import FakeUserAgent
 
-from .errors import MusicError
-from .song import Album, Song
-
-ua = FakeUserAgent()
-session = requests.session()
-client_id = "LBCcHmRB8XSStWL6wKH2HPACspQlXg2P"
-song_url = "https://soundcloud.com/rapviet_viechannel/rolling-down-feat-captain"
-playlist_url = "https://soundcloud.com/rapviet_viechannel/sets/rap-vi-t-2023-t-p-13"
-album_url = "https://soundcloud.com/user-443192601/sets/nhac-phim-mua-viet-tinh-ca"
+if TYPE_CHECKING:
+    from requests import Response
 
 
-headers = {
-    "Accept": "application/json, text/javascript, */*; q=0.01",
-    "Host": "api-widget.soundcloud.com",
-    "User-Agent": ua.random
-}
+class SoundCloud:
+    def __init__(self) -> None:
+        self.BASE_URL = "https://api-v2.soundcloud.com"
+        self.ua = FakeUserAgent().random
+        self.session = requests.Session()
+        self.headers = {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9,vi;q=0.8",
+            "Connection": "keep-alive",
+            "Host": "api-v2.soundcloud.com",
+            "User-Agent": self.ua,
+        }
+        self.client_id = "LBCcHmRB8XSStWL6wKH2HPACspQlXg2P"
+        self.app_version = "1692966083"
 
-
-def resolve_url(url: str) -> Dict[str, Any]:
-    res = session.get(url, headers=headers)
-    if res.status_code == 200:
-        return res.json()
-    else:
-        raise MusicError("SoundCloud: Cannot resolve the given URL.")
-
-
-def get_playback_url(track: Dict[str, Any]):
-    stream_url = track["media"]["transcodings"][0]["url"]
-    track_authorization = track["track_authorization"]
-    audio_url = f"{stream_url}?client_id={client_id}&track_authorization={track_authorization}"
-
-    # Transcoding involves 3 types of protocols: HLS, progressive and Opus.
-    # We prefer to use the 'HLS' protocol
-    response = session.get(url=audio_url, headers=headers)
-    playback_url = response.json()
-    return playback_url["url"]
-    # return audio_url
-
-
-# The response data will be in one of the following forms: 'track' or 'playlist'.
-# Additionally, the term 'playlist' also encompasses albums.
-# The specific format of the response data will determine the type of responsive function we need to create.
-def format_duration(duration):
-    duration_fmt = datetime.timedelta(milliseconds=float(duration))
-    formatted_duration = str(duration_fmt - datetime.timedelta(microseconds=duration_fmt.microseconds))
-    return formatted_duration
-
-
-def format_playback_count(playback_count) -> str:
-    view_count_fmt = int(playback_count)
-    return "{:,}".format(view_count_fmt)
-
-
-def format_upload_date(date):
-    datetime_obj = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
-    return datetime_obj.strftime("%d/%m/%Y")
-
-
-def get_thumbnail(track):
-    if track["artwork_url"] is None:
-        thumbnail = track["user"]["avatar_url"]
-    else:
-        thumbnail = track["artwork_url"]
-
-    return thumbnail
-
-
-def get_data(soundcloud_url, ctx):
-    url = f"https://api-widget.soundcloud.com/resolve?url={soundcloud_url}&format=json&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P&app_version=1692966083"
-    response_data = resolve_url(url)
-    kind = response_data["kind"]
-    if kind == "track":
-        track = response_data
-        song = Song(
-            title=track["title"],
-            playback_url=get_playback_url(track),
-            uploader=track["user"]["username"],
-            duration=format_duration(track["duration"]),
-            playback_count=format_playback_count(track["playback_count"]),
-            upload_date=format_upload_date(track["created_at"]),
-            thumbnail=get_thumbnail(track),
-            webpage_url=track["permalink_url"],
-            category=track["genre"],
-            album=None,
-            context=ctx
+    async def make_request(self, url) -> "Response":
+        loop = asyncio.get_event_loop()
+        future = loop.run_in_executor(
+            None, functools.partial(self.session.get, headers=self.headers, url=url)
         )
-        return song
-    else:
-        playlist_id = response_data["id"]
-        track_ids = ",".join([str(track["id"]) for track in response_data["tracks"]])
-        playlist_name = response_data["title"]
-        get_track_url = f"https://api-widget.soundcloud.com/tracks?ids={track_ids}&playlistId={playlist_id}&playlistSecretToken&format=json&client_id={client_id}"
-        response = session.get(get_track_url, headers=headers)
-        tracks = response.json()
+        response = await future
+        return response
 
-        songs = [Song(
-                title=track["title"],
-                playback_url=get_playback_url(track),
-                uploader=track["user"]["username"],
-                duration=format_duration(track["duration"]),
-                playback_count=format_playback_count(track["playback_count"]),
-                upload_date=format_upload_date(track["created_at"]),
-                thumbnail=get_thumbnail(track),
-                webpage_url=track["permalink_url"],
-                category=track["genre"],
-                album=Album(playlist_name),
-                context=ctx
+    def search(self, query: str):
+        search = f"{self.BASE_URL}/search/tracks?q={query}&client_id={self.client_id}&app_version={self.app_version}"
+        response = self.session.get(search, headers=self.headers)
+        return {
+            "total_results": response.json()["total_results"],
+            "tracks": response.json()["collection"]
+        }
+
+    def resolve_url(self, url: str):
+        resolve = f"{self.BASE_URL}/resolve?url={url}&format=json&client_id={self.client_id}&app_version={self.app_version}"
+        response = self.session.get(resolve, headers=self.headers)
+        return response.json()
+
+    def get_thumbnail(self, track) -> str:
+        return track["artwork_url"] or track["user"]["avatar_url"]
+
+    async def get_playback_url(self, track: Dict[str, Any]):
+        # Transcoding involves 3 types of protocols: HLS, progressive and Opus.
+        # We prefer to use the 'HLS' protocol because it's the best for streaming.
+        stream_url = track["media"]["transcodings"][0]["url"]
+        track_authorization = track["track_authorization"]
+        audio_url = f"{stream_url}?client_id={self.client_id}&track_authorization={track_authorization}"
+        response = await self.make_request(audio_url)
+        playback_url = response.json()
+        return playback_url["url"]
+
+    async def get_playlist_tracks(self, track_ids: list[int], playlist_id: int):
+        MAX_TRACKS_PER_REQUEST = 50
+        urls = []
+        results = []
+
+        if len(track_ids) > MAX_TRACKS_PER_REQUEST:
+            chunked_tracks = [
+                track_ids[i : i + MAX_TRACKS_PER_REQUEST]
+                for i in range(0, len(track_ids), MAX_TRACKS_PER_REQUEST)
+            ]
+            for chunk in chunked_tracks:
+                quoted_track_ids = ",".join(map(str, chunk))
+                urls.append(
+                    f"{self.BASE_URL}/tracks?ids={quoted_track_ids}&playlistId={playlist_id}&playlistSecretToken&format=json&client_id={self.client_id}"
+                )
+        else:
+            quoted_track_ids = ",".join(map(str, track_ids))
+            urls.append(
+                f"{self.BASE_URL}/tracks?ids={quoted_track_ids}&playlistId={playlist_id}&playlistSecretToken&format=json&client_id={self.client_id}"
             )
-            for track in tracks
-        ]
-        return songs
 
-# data = get_data()
-# if isinstance(data, list):
-#     for song in data:
-#         print(song)
-# else:
-#     print(data)
+        responses: list[Response] = await asyncio.gather(
+            *(self.make_request(url) for url in urls)
+        )
+        for res in responses:
+            results += res.json()
+
+        for i in range(len(track_ids)):
+            if track_ids[i] == results[i]["id"]:
+                continue
+            idx1 = i
+            idx2 = [track["id"] for track in results].index(track_ids[i])
+            results[idx1], results[idx2] = results[idx2], results[idx1]
+
+        return results
+
+    async def extract_song(self, url: str):
+        resolve = self.resolve_url(url)
+        if resolve["kind"] == "playlist":
+            tracks = resolve["tracks"]
+            playlist_id = resolve["id"]
+            track_ids = [track["id"] for track in tracks]
+            return {
+                "playlist_id": playlist_id,
+                "playlist_name": resolve["title"],
+                "tracks": await self.get_playlist_tracks(track_ids, playlist_id),
+            }
+        else:
+            return {
+                "playlist_id": None,
+                "playlist_name": None,
+                "tracks": [resolve],
+            }
