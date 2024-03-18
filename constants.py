8b49e991d568d6c56a1d217f58e89f08df139a96
Tuấn Anh <phantuananh0202@gmail.com>
2024-03-19 00:23:24 +0700

    Remove unused constants and options

--- constants.py
+++ constants.py
@@ -3,18 +3,6 @@ import os
 # declare some constants
 CUR_PATH = os.path.dirname(os.path.realpath(__file__))
 TEMP_FOLDER = CUR_PATH + r"/temp_folder"
-BOT_DATA_FOLDER = CUR_PATH + r"/bot_data"
-GAMEFREE_CHANNEL_ID = 916965073765957672
 
 VOICE_TIMEOUT = 10 * 60
 RENEW_TIME = 6 * 60 * 60
-
-YDL_OPTS = {
-    "format": "bestaudio/best",
-    "quiet": True,
-}
-
-FFMPEG_OPTIONS = {
-    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-    "options": "-vn",
-}
