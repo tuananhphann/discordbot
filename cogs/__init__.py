f2683229cd4d9d875def71d92c17202f0b1feaed
Tuấn Anh <phantuananh0202@gmail.com>
2024-03-19 00:22:55 +0700

    Add .idea/ and table.py to .gitignore, and refactor cogs/__init__.py

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,15 +1,12 @@
 import logging
-import constants
 import os
 
-from .music.controller import *
-from .music.playlist import *
-from .music.search import *
-from .music.music import *
+import constants
+
+_log = logging.getLogger(__name__)
+_log.addHandler(logging.NullHandler())
 
 try:
     os.mkdir(constants.TEMP_FOLDER)
-    os.mkdir(constants.BOT_DATA_FOLDER)
-except: None
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+except FileExistsError:
+    pass
