c27f4a21e9a2ba784529d94fc4666edfc05b09b9
Tuấn Anh <phantuananh0202@gmail.com>
2025-01-19 02:12:57 +0700

    feat: Implement YouTubeService for retrieving PoToken using Selenium

---
+++ cogs/music/services/youtube/service.py
@@ -0,0 +1,39 @@
+import json
+import logging
+
+from patterns.singleton import SingletonMeta
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from seleniumwire import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+_log = logging.getLogger(__name__)
+
+
+class YoutubeService(metaclass=SingletonMeta):
+
+    @staticmethod
+    def getPoToken() -> tuple[str, str]:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+
+        driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()), options=options
+        )
+        _log.info("YouTube: Getting PoToken...")
+        driver.get("https://www.youtube.com/embed/aqz-KE-bpKQ")
+        driver.find_element(By.ID, "player").click()
+        body = None
+
+        for request in driver.requests:
+            if "v1/player" in request.url:
+                body = request._body
+                break
+
+        if body is not None:
+            data = json.loads(body)
+            visitorData = data["context"]["client"]["visitorData"]
+            poToken = data["serviceIntegrityDimensions"]["poToken"]
+        _log.info("YouTube: PoToken is got")
+        driver.close()
+        return visitorData, poToken
